f8ba37c6181bcefc74fb2b89048d7d8856ddf9df
RafiaChy <rafiachowdhury24@gmail.com>
2022-01-07 20:13:29 +0600

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 
-img = cv2.imread('baboon.jpg', 0)
-cv2.imshow('Sample ', img)
+img = cv2.imread('baboon.jpg', 1)
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow('Sample ', rgb)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
